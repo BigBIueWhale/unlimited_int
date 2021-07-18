2fbbcf193106224094ce0da02e177c537ee2cd1f
BigBIueWhale <85248530+BigBIueWhale@users.noreply.github.com>
2021-07-18 23:53:21 +0300

    Added operator= and compliance with namespace

--- int_array_list.h
+++ int_array_list.h
@@ -1,20 +1,27 @@
-#ifndef INT_ARRAY_LIST_FILE_DEFINED
-#define INT_ARRAY_LIST_FILE_DEFINED true
+#ifndef UNLIMITED_INT_ARRAY_LIST_H
+#define UNLIMITED_INT_ARRAY_LIST_H true
 #include "ListArrays.h"
-class int_array_list
+namespace unlimited
 {
-public:
-	ListArrays intarrays;
-	many_bits num_of_ints;
-	int_array_list() { this->num_of_ints = 0; }
-	void flush(); //flush is the same as destroy
-	void print_properties();
-	void swap(int_array_list& int_array_list_to_swap_with);
-	void append(int_array_list&);
-	void prepend(int_array_list&);
-	__list_location__ find_num_of_int_from_insignificant(many_bits num_int_to_find);
+	class int_array_list
+	{
+	public:
+		ListArrays intarrays;
+		many_bits num_of_ints;
+		int_array_list() { this->num_of_ints = 0; }
+		void operator=(const int_array_list& num_to_assign);
+		void flush(); //flush is the same as destroy
 #if DEBUG_MODE > 0
-	bool find_inconsistencies();
+		void print_properties() const;
 #endif
-};
+		void swap(int_array_list& int_array_list_to_swap_with);
+		void append(int_array_list&);
+		void prepend(int_array_list&);
+		//finds the int index from the insignificant side. Ignores num_used_ints.
+		__list_location__ find_num_of_int_from_insignificant(many_bits num_int_to_find);
+#if DEBUG_MODE > 0
+		bool find_inconsistencies() const;
+#endif
+	};
+}
 #endif
