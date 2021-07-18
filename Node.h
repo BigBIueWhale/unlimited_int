eeef6384bd4c8b8bd06ab356a2649d857675c0be
BigBIueWhale <85248530+BigBIueWhale@users.noreply.github.com>
2021-07-18 23:47:44 +0300

    Added compliance with namespace

--- Node.h
+++ Node.h
@@ -1,44 +1,47 @@
-#ifndef NODEOFLISTARRAYS_HEADER
-#define NODEOFLISTARRAYS_HEADER true
+#ifndef UNLIMITED_NODE_H
+#define UNLIMITED_NODE_H
 #include "int_array.h"
-struct Node
+namespace unlimited
 {
-	Node* previous;
-	Node* next;
-	int_array* value;
-	Node()
+	struct Node
 	{
-		this->previous = nullptr;
-		this->next = nullptr;
-		this->value = nullptr;
-	}
-	Node(const Node& la) { this->operator=(la); }
-	Node(int_array* value)
-	{
-		this->value = value;
-		this->next = nullptr;
-		this->previous = nullptr;
-	}
-	void operator=(const Node& la)
-	{
-		this->previous = la.previous;
-		this->next = la.next;
-		this->value = la.value;
-	}
-	void swap(Node& other)
-	{
-		Node* temp_Node = other.previous;
-		other.previous = this->previous;
-		this->previous = temp_Node;
+		Node* previous;
+		Node* next;
+		int_array* value;
+		Node()
+		{
+			this->previous = nullptr;
+			this->next = nullptr;
+			this->value = nullptr;
+		}
+		Node(const Node& la) { this->operator=(la); }
+		Node(int_array* value)
+		{
+			this->value = value;
+			this->next = nullptr;
+			this->previous = nullptr;
+		}
+		void operator=(const Node& la)
+		{
+			this->previous = la.previous;
+			this->next = la.next;
+			this->value = la.value;
+		}
+		void swap(Node& other)
+		{
+			Node* temp_Node = other.previous;
+			other.previous = this->previous;
+			this->previous = temp_Node;
 
-		temp_Node = other.next;
-		other.next = this->next;
-		this->next = temp_Node;
+			temp_Node = other.next;
+			other.next = this->next;
+			this->next = temp_Node;
 
-		int_array* temp_int_array;
-		temp_int_array = other.value;
-		other.value = this->value;
-		this->value = temp_int_array;
-	}
-};
+			int_array* temp_int_array;
+			temp_int_array = other.value;
+			other.value = this->value;
+			this->value = temp_int_array;
+		}
+	};
+}
 #endif
