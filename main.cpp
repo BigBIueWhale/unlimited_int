a4e2c058e7952b42525529f3e4ba354e05ae2dd0
BigBIueWhale <85248530+BigBIueWhale@users.noreply.github.com>
2021-07-19 12:49:55 +0300

    Example now takes advantage of std::shared_ptr

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ void helper();
 int main(int argc, char* argv[])
 {
 	helper();
+
 #if (DEBUG_MODE > 0) || (DEBUG_MODE == -2)
 	unlimited::delete_all_static_memory();
 	std::cout << "\n\nDEBUG INFORMATION:\n" << std::dec;
@@ -18,38 +19,38 @@ int main(int argc, char* argv[])
 	return 0;
 }
 using namespace unlimited;
-unlimited_int* Fibonacci(unsigned long long index);
+std::shared_ptr<unlimited_int> Fibonacci(unsigned long long index);
 void helper()
 {
 	std::cout << "\nEnter the number of bits for prime number: ";
 	int bits;
 	std::cin >> bits;
-	unlimited_int prime = unlimited_int::generate_random_prime(unlimited_int::pow(2, bits - 1), unlimited_int(unlimited_int::pow(2, bits)) - 1);
+	unlimited_int prime = unlimited_int::generate_random_prime(*unlimited_int::pow(2, bits - 1), *unlimited_int::pow(2, bits) - 1);
 	std::cout << "\nprime number calculated.\nIn hexadecimal: " << std::hex << prime << "\nIn decimal: " << std::dec << prime;
-	std::cout << "\nSHA256 of prime number: " << std::hex << unlimited_int(prime.calculate_sha256_hash()) << std::dec;
-	std::cout << "\nSHA512 of prime number: " << std::hex << unlimited_int(prime.calculate_sha512_hash()) << std::dec;
-	std::cout << "\nRandom number between 0 and the prime number (including): " << unlimited_int(prime.generate_random(0, prime));
+	std::cout << "\nSHA256 of prime number: " << std::hex << *prime.calculate_sha256_hash() << std::dec;
+	std::cout << "\nSHA512 of prime number: " << std::hex << *prime.calculate_sha512_hash() << std::dec;
+	std::cout << "\nRandom number between 0 and the prime number (including): " << *unlimited_int::generate_random(0, prime);
 	std::cout << "\nThe random number in base 36: " << prime.to_string(36);
-	std::cout << "\nThe greatest common divisor of two random numbers: " << unlimited_int(unlimited_int::gcd(unlimited_int::generate_random(0, 1000), unlimited_int::generate_random(0, 1000)));
-	std::cout << "\nThe lowest common multiple of two random numbers: " << unlimited_int(unlimited_int::lcm(unlimited_int::generate_random(0, 1000), unlimited_int::generate_random(0, 1000)));
+	std::cout << "\nThe greatest common divisor of two random numbers: " << *unlimited_int::gcd(unlimited_int::generate_random(0, 1000), unlimited_int::generate_random(0, 1000));
+	std::cout << "\nThe lowest common multiple of two random numbers: " << *unlimited_int::lcm(unlimited_int::generate_random(0, 1000), unlimited_int::generate_random(0, 1000));
 	unsigned long long fib_index;
 	std::cout << "\n\nEnter the index to find in Fibonacci sequence (where index 1 is 0 and index 2 is 1 etc.): ";
 	std::cin >> fib_index;
-	std::cout << "\nFibonacci num in hex: " << std::hex << unlimited_int(Fibonacci(fib_index));
+	std::cout << "\nFibonacci num in hex: " << std::hex << *Fibonacci(fib_index);
 	std::string line;
 	std::cout << "\nEnter positive or negative integer in base 10: ";
 	std::cin.ignore();
 	std::getline(std::cin, line);
-	std::cout << "\nThat same number squared: " << std::dec << unlimited_int(unlimited_int(unlimited_int::from_string(line)).power2());
+	std::cout << "\nThat same number squared: " << *unlimited_int::from_string(line)->power2();
 }
-unlimited_int* Fibonacci(unsigned long long index)
+std::shared_ptr<unlimited_int> Fibonacci(unsigned long long index)
 {
 	if (index == 0)
 		throw "index 0 is invalid";
 	else if (index == 1)
-		return unlimited_int(0).copy();
+		return std::shared_ptr<unlimited_int>(unlimited_int(0).copy());
 	else if (index == 2)
-		return unlimited_int(1).copy();
+		return std::shared_ptr<unlimited_int>(unlimited_int(1).copy());
 	unlimited_int a = 1, b = 0;
 	bool a_is_bigger = true;
 	for (unsigned long long counter = 3; counter <= index; ++counter)
@@ -71,7 +72,7 @@ unlimited_int* Fibonacci(unsigned long long index)
 	}
 	std::cout << "\n";
 	if (a_is_bigger)
-		return a.copy();
+		return std::shared_ptr<unlimited_int>(a.copy());
 	else
-		return b.copy();
+		return std::shared_ptr<unlimited_int>(b.copy());
 }
